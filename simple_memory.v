0acbb716e7c8a6ce14bb416a92565c13505df392
Icenowy Zheng <icenowy@aosc.io>
2018-12-11 01:13:37 +0800

    Use non-pending assignment in simple_memory
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- simple_memory.v
+++ simple_memory.v
@@ -25,20 +25,20 @@ end
 
 always @(negedge iwClk or negedge iwnRst) begin
 	if (!iwnRst) begin
-		rMemory[pWords * 4 - 1] = 0;
-		rMemory[pWords * 4 - 2] = 0;
-		rMemory[pWords * 4 - 3] = 0;
-		rMemory[pWords * 4 - 4] = 0;
+		rMemory[pWords * 4 - 1] <= 0;
+		rMemory[pWords * 4 - 2] <= 0;
+		rMemory[pWords * 4 - 3] <= 0;
+		rMemory[pWords * 4 - 4] <= 0;
 		`include "simple_memory_rom.v"
 	end else begin
 		if (iwWstrb & 4'b1)
-			rMemory[iwWriteAddr] = iwWriteData[7:0];
+			rMemory[iwWriteAddr] <= iwWriteData[7:0];
 		if (iwWstrb & 4'b10)
-			rMemory[iwWriteAddr + 1] = iwWriteData[15:8];
+			rMemory[iwWriteAddr + 1] <= iwWriteData[15:8];
 		if (iwWstrb & 4'b10)
-			rMemory[iwWriteAddr + 2] = iwWriteData[23:16];
+			rMemory[iwWriteAddr + 2] <= iwWriteData[23:16];
 		if (iwWstrb & 4'b1000)
-			rMemory[iwWriteAddr + 3] = iwWriteData[31:24];
+			rMemory[iwWriteAddr + 3] <= iwWriteData[31:24];
 	end
 
 	orReadData[7:0] = rMemory[iwReadAddr];
